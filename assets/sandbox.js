7cd88f0cff2c94f8f1c2cf399f5fa9ba95254096
Caitlyn Griffing <caitlyn.griffing@gmail.com>
2022-01-29 04:11:40 -0600

    created timeblock in JavaScript

--- assets/sandbox.js
+++ assets/sandbox.js
@@ -1,19 +1,106 @@
-// current day, date, time displayed at the top of calendar
+// current [day, date, time] displayed at the top of calendar
 $("#currentDay").text(moment().format("llll"));
 
-// present timeblocks for standard business hours
-let container = $(".container");
+/* 
+container
+    timeblock row
+        hour box
+        description (text) box
+        save button 
+------------------------------------
+timeblock div row == blockRow
+
+hour div : hour of day | AM or PM
+
+description div : textarea
+
+save button : save icon
+*/
+
+// TODO 1.0 --- Present timeblocks for standard business hours
+// business hours: 9-5
+
+// TODO Create timeblock rows  ---  COMPLETED
+
+// TODO: 1.B - Create hour box and input different hours of the day ( am & pm )
+// created hour box div  but HAVE NOT created the inner text content
+// 
+
+// TODO 1.C --- COMPLETED - Create a textarea box and place inside a div.discription box
+// created description box
+// created textarea box
+
+// TODO 1.D --- COMPLETED - Create Save button with Icon
+
+// TODO extra (missed) --- COMPLETED - append save icon and button
+
+// TODO 1.E --- COMPLETED - Append hour div, description div, and save button TO the timeblock row
+
+// TODO 1.F --- COMPLETED - Append timeblockrow to .container in the HTML
+
+// TODO extra (missed) --- COMPLETED - append textarea to description
+
+// The attr({}) method also allows you to set multiple attributes at the same time.
+
+// timeblock row div
+var blockRowDiv = $("<div>").attr({
+    "class": "row"
+});
+
+// append timeblock row div to the container class in HTML
+$(".container").append(blockRowDiv);
+
+// hour div box
+var hourDiv = $("<div>").attr({
+    "class": "col-md-1 hour"
+});
+
+// description div box  ---  make sure to delete the past class
+var descriptionDiv = $("<div>").attr({
+    "class": "col-md-10 past description p-0"
+});
+
+// text area - data
+var textData = $("<textarea>");
+
+descriptionDiv.append(textData);
+
+// save icon
+var saveIcon = $("<i class='far fa-save fa-lg'></i>");
+
+// save button
+var saveButton = $("<button>").attr({
+    "class": "col-md-1 saveBtn"
+})
+
+// append save icon and button
+saveButton.append(saveIcon);
+
+blockRowDiv.append(hourDiv, descriptionDiv, saveButton);
 
 
-// .row : 80px height - prewrap (wrap text content inside)
-// .timebock : 15px border radius
-// textarea : transparent - font black
+// GIVEN I am using a daily planner to create a schedule
 
-// WHAT IS INSIDE THE TIMEBLOCKS
-// .time-block.row
-// textarea.description
-// (.past | .present | .future) colors for timeblock rows
+//* COMPLETED *//
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
 
-// planner hour
-// save button (imported from google fonts)
 
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+
+
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
+
+// WHEN I click into a timeblock
+// THEN I can enter an event
+
+
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+
+
+// WHEN I refresh the page
+// THEN the saved events persist
